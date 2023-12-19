6d289db0a1efed131c79329ac1e82169a6a5d26d
cpmoore0317 <kingpkayy@gmail.com>
2023-12-18 18:53:27 -0600

    Testing Chris's script

--- public/scripts/startup.js
+++ public/scripts/startup.js
@@ -1,22 +1,29 @@
-// startup.js
+// Initialize firestore and querySelector variables
 
 const firestore = firebase.firestore();
 const startButton = document.querySelector('.startButton');
 const promptDiv = document.querySelector('.prompt');
-const rChoiceDivs = document.querySelectorAll('.rbutton-text');
+const sChoiceButton = document.querySelector('.sbutton');
 const sChoiceDivs = document.querySelectorAll('.sbutton-text');
-const buttonContainer = document.querySelector('.button_container');
-const startButtonContainer = document.querySelector('.startButton_container');
-
-// Initially hide the button container
-buttonContainer.style.display = 'none';
+const rChoiceButton = document.querySelector('.rbutton');
+const rChoiceDivs = document.querySelectorAll('.rbutton-text');
+let currentPromptId;
+let currentRChoiceId;
+let currentSChoiceId;
 
 startButton.addEventListener('click', async () => {
   try {
-    // Retrieve documents from FireStore
+    // Retrieve Prompt from FireStore
     const promptDoc = await firestore.collection('prompts').doc('prompt_1').get();
-    const rChoiceDoc = await firestore.collection('r_choices').doc('r_choice_1').get();
-    const sChoiceDoc = await firestore.collection('s_choices').doc('s_choice_1').get();
+
+    // Store current Prompt and Choice IDs
+    currentPromptId = promptDoc.data().id;
+    currentRChoiceId = promptDoc.data().r_choice_id;
+    currentSChoiceId = promptDoc.data().s_choice_id;
+
+    // Retrieve Choices based on Current Prompt
+    const rChoiceDoc = await firestore.collection('r_choices').doc(currentRChoiceId).get();
+    const sChoiceDoc = await firestore.collection('s_choices').doc(currentSChoiceId).get();
 
     // Access the text fields from the documents' data
     const promptText = promptDoc.data().text;
@@ -30,14 +37,71 @@ startButton.addEventListener('click', async () => {
     rChoiceDivs.forEach(div => div.textContent = rChoiceText);
     sChoiceDivs.forEach(div => div.textContent = sChoiceText);
 
-    // Show the button container
-    buttonContainer.style.display = 'flex';
-    
-    // Remove start button
-    startButtonContainer.style.display = 'none';
+  } catch (error) {
+    console.error('Error retrieving data from Firestore:', error);
+  }
+});
+
+sChoiceButton.addEventListener('click', async () => {
+  try {
+    // Retrieve next Prompt based on s_choice
+    const selectedSChoice = await firestore.collection('s_choices').doc(currentSChoiceId).get();
+    const promptDoc = await firestore.collection('prompts').doc(selectedSChoice.data().prompt_id).get();
+
+    // Update Prompt and Choice IDs
+    currentPromptId = promptDoc.data().id;
+    currentRChoiceId = promptDoc.data().r_choice_id;
+    currentSChoiceId = promptDoc.data().s_choice_id;
+
+    // Retrieve Choices based on Current Prompt
+    const rChoiceDoc = await firestore.collection('r_choices').doc(currentRChoiceId).get();
+    const sChoiceDoc = await firestore.collection('s_choices').doc(currentSChoiceId).get();
+
+    // Access the text fields from the documents' data
+    const promptText = promptDoc.data().text;
+    const rChoiceText = rChoiceDoc.data().text;
+    const sChoiceText = sChoiceDoc.data().text;
+
+    // Update the text in the prompt
+    promptDiv.textContent = promptText;
+
+    // Update the text in the buttons
+    rChoiceDivs.forEach(div => div.textContent = rChoiceText);
+    sChoiceDivs.forEach(div => div.textContent = sChoiceText);
 
   } catch (error) {
     console.error('Error retrieving data from Firestore:', error);
   }
 });
 
+rChoiceButton.addEventListener('click', async () => {
+  try {
+    // Retrieve next Prompt based on r_choice
+    const selectedRChoice = await firestore.collection('r_choices').doc(currentRChoiceId).get();
+    const promptDoc = await firestore.collection('prompts').doc(selectedRChoice.data().prompt_id).get();
+
+    // Update Prompt and Choice IDs
+    currentPromptId = promptDoc.data().id;
+    currentRChoiceId = promptDoc.data().r_choice_id;
+    currentSChoiceId = promptDoc.data().s_choice_id;
+
+    // Retrieve Choices based on Current Prompt
+    const rChoiceDoc = await firestore.collection('r_choices').doc(currentRChoiceId).get();
+    const sChoiceDoc = await firestore.collection('s_choices').doc(currentSChoiceId).get();
+
+    // Access the text fields from the documents' data
+    const promptText = promptDoc.data().text;
+    const rChoiceText = rChoiceDoc.data().text;
+    const sChoiceText = sChoiceDoc.data().text;
+
+    // Update the text in the prompt
+    promptDiv.textContent = promptText;
+
+    // Update the text in the buttons
+    rChoiceDivs.forEach(div => div.textContent = rChoiceText);
+    sChoiceDivs.forEach(div => div.textContent = sChoiceText);
+
+  } catch (error) {
+    console.error('Error retrieving data from Firestore:', error);
+  }
+});
